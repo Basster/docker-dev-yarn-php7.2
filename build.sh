5250e9023f9c4c1c92e8f1baaa91f22de7db08e1
Ole Rößner <o.roessner@neusta.de>
2019-02-21 13:37:02 +0100

    raise version to 7.2.1

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build . -t djbasster/dev-yarn-php:7.2
-docker push djbasster/dev-yarn-php:7.2
+docker build . -t djbasster/dev-yarn-php:7.2.1
+docker push djbasster/dev-yarn-php:7.2.1
